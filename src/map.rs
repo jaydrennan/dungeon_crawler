95410194e31ebfbc4a4074d453a78b42730eec18
jaydrennan <jsdman1313@gmail.com>
2024-01-10 14:12:51 -0800

    initial commit

---
+++ src/map.rs
@@ -0,0 +1,18 @@
+use crate::prelude::*;
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy,Clone,PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map{
+    pub fn new()->Self{
+        Self{}
+    }
+}
