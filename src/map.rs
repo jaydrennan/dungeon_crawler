2c6a5457626b76b50e917dc6a3cf0ebf09ce3b78
jaydrennan <jsdman1313@gmail.com>
2024-01-15 21:25:00 -0800

    adds player struct and rules

--- src/map.rs
+++ src/map.rs
@@ -29,20 +29,24 @@ impl Map {
                     TileType::Floor => {
                         ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
                     }
-                    TileType::Wall=>{
-                        ctx.set(x,y,GREEN,BLACK, to_cp437('#'));
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
                     }
                 }
             }
         }
     }
 
-    pub fn in_bonds(&self, point: Point)->bool{
-        point.x >=0 && point.x < SCREEN_WIDTH
-            && point.y >= 0 && point.y < SCREEN_HEIGHT
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
     }
-    pub fn try_idx(&self, point : Poiint )-> Option<usize>{
-        if !self.in_bounds(point){
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
             None
         } else {
             Some(map_idx(point.x, point.y))
